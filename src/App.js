7304ea9b85a09fe621ae668cff0ad7aa91e7a6c2
Mahantesh <mahantesh0112@gmail.com>
2019-11-06 12:11:45 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import request from 'superagent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    componentDidMount() {
+        request
+            .get('https://api.github.com/users/mahanteshshiva')
+            .then(res => res.json())
+    }
+
+
+render() {
+    return (
+        <h1>kdhflj</h1>
+    );
+}
+
 }
 
 export default App;
